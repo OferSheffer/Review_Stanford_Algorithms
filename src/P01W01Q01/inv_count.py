76e27e08ad744448ec23eee70a61583b3d187dac
OferSheffer <ofer.sheffer@gmail.com>
2015-10-14 23:08:03 +0300

    Add local files

---
+++ src/P01W01Q01/inv_count.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 14, 2015
+@author: Ofer
+
+Question 1
+Download the text file here. (Right click and save link as) 
+http://spark-public.s3.amazonaws.com/algo1/programming_prob/IntegerArray.txt
+
+This file contains all of the 100,000 integers between 1 and 100,000 (inclusive)
+ in some order, with no integer repeated. Your task is to compute the number of 
+ inversions in the file given, where the ith row of the file indicates the ith 
+ entry of an array. Because of the large size of this array, you should 
+ implement the fast divide-and-conquer algorithm covered in the video lectures.
+'''
+
+
+
+if __name__ == '__main__':
+    pass
