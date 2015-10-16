f6e15145d5be342d03b509e8c65f49141b0d342e
OferSheffer <ofer.sheffer@gmail.com>
2015-10-16 21:31:44 +0300

    Add Sort_and_Count function

--- src/P01W01Q01/inv_count.py
+++ src/P01W01Q01/inv_count.py
@@ -13,6 +13,35 @@ This file contains all of the 100,000 integers between 1 and 100,000 (inclusive)
  implement the fast divide-and-conquer algorithm covered in the video lectures.
 '''
 
+'''
+Educational Notes:
+ Concatenating immutable sequences always results in a new object. This means
+ that building up a sequence by repeated concatenation will have a quadratic
+ runtime cost in the total sequence length - e.g. like the question about
+ repeated merge-sorts on k sequences of length n. To get a linear runtime 
+ cost see:  https://docs.python.org/3/library/stdtypes.html
+ 
+Python TimeComplexity: 
+ https://wiki.python.org/moin/TimeComplexity
+
+ list - insert is a terrible idea. O(n)
+ set item - good idea. O(1)
+'''
+
+def Sort_and_Count(A, n, start_index=0):
+    """
+    Input: array A, length n, start_index(default=0)
+    Output: inversions (values where i<j, but A[i]>A[j])
+    Side Effect: A is sorted.
+    """
+    if n=1, return 0    # base case, array of length 1
+    else
+        x=Sort_and_Count(A, n/2, start_index)           # Input 1st half of A
+        y=Sort_and_Count(A, n/2+n%2, start_index+n/2)   # Input 2nd half of A
+        
+        z=Merge_and_CountSplitInv(B,C,n)    # sorted 1st and 2nd halves
+        
+    return x+y+z
 
 
 if __name__ == '__main__':
