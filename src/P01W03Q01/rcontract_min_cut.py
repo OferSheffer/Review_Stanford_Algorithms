0e8f8e5f5cc0bc8f8c96347ee0c6ce03739f6f66
OferSheffer <ofer.sheffer@gmail.com>
2015-11-01 23:04:59 +0200

    construct skeleton

--- src/P01W03Q01/rcontract_min_cut.py
+++ src/P01W03Q01/rcontract_min_cut.py
@@ -4,7 +4,7 @@ Created on Nov 1, 2015
 @author: Ofer
 
 Question 1
-Download the text file here. (Right click and save link as) 
+Download the text file here. (Right click and save link as)
 http://spark-public.s3.amazonaws.com/algo1/programming_prob/kargerMinCut.txt
 
 The file contains the adjacency list representation of a simple undirected
@@ -18,7 +18,7 @@ This just means that the vertex with label 6 is adjacent to
 
 Your task is to code up and run the randomized contraction algorithm for the
 min cut problem and use it on the above graph to compute the min cut
-(i.e., the minimum-possible number of crossing edges) 
+(i.e., the minimum-possible number of crossing edges)
 
 (HINT: Note that you'll have to figure out an implementation of edge
 contractions. Initially, you might want to do this naively, creating a new
@@ -35,7 +35,73 @@ Educational notes:
 '''
 
 from random import randrange
+import sys
+import unittest
+
+REPETITION = 200  # n^n*ln(n)
 
 irand = randrange(0, 10)
 
 
+def rcontract_min_cut(o_data):
+    # TODO: create this algorithm
+    return 0
+    pass
+
+
+def get_min_cut(v_data, e_data):
+    min_cut = None
+    for _ in range(REPETITION):
+        min_cut = rcontract_min_cut(v_data, e_data)
+    return min_cut
+
+
+def get_graph_data(adjacency_data):
+    # TODO: implement
+    return "", ""
+
+
+class QuickSortTestCase(unittest.TestCase):
+    """Tests for `rcontract_min_cut.py`"""
+
+    def test_get_graph_data(self):
+        adjacency_data = [
+                          "",
+                          "",
+                          "",
+                          ]
+        v_data, e_data = get_graph_data(adjacency_data)
+
+        self.assertTrue(v_data="", "Not yet implemented.")
+        self.assertTrue(e_data="", "Not yet implemented.")
+        self.assertTrue(None, "Not yet implemented.")
+        pass
+
+    def test_somthing(self):
+        # TODO: implement some tests
+        pass
+
+
+def main(file_name):
+    # take values from file and run quick_sort
+    with open(file_name) as fh:
+        if file_name[:4] == 'test':
+            print((fh.readline()).strip())  # remove+show answer from test file
+        adjacency_data = [line.strip() for line in fh]
+        for line in adjacency_data:
+            v_data, e_data = get_graph_data(adjacency_data)
+
+        min_cut = get_min_cut(v_data, e_data)
+
+        print(min_cut)
+
+
+if __name__ == '__main__':
+    # working with argv to accept file input
+    if len(sys.argv) > 2:
+        sys.exit("Usage: inv_count <file_name> (leave empty for testing)")
+    if len(sys.argv) == 1:
+        print("No filename input, testing...")
+        unittest.main()
+    # else: argv == 2
+    main(sys.argv[1])
