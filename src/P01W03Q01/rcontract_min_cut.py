0817a9fa68da265f0a053ef05f5e0abb4a916ed2
OferSheffer <ofer.sheffer@gmail.com>
2015-11-03 13:40:44 +0200

    Graph -> MultigGraph (fix graph init to avoid dual listing of edges)

--- src/P01W03Q01/rcontract_min_cut.py
+++ src/P01W03Q01/rcontract_min_cut.py
@@ -37,21 +37,18 @@ Educational notes:
 
 '''
 
-from random import randrange
+from random import choice as rchoice
+from math import ceil, log
 import networkx as nx
 import sys
 import unittest
 
-# from graph import Graph
 
-
-REPETITION = 200  # n^n*ln(n)
-
-irand = randrange(0, 10)
-
-
-class ExtendedGraph(nx.Graph):
+class ExtendedMultiGraph(nx.MultiGraph):
     """
+    MultiGraph - graph class that allows multiple undirected edges between
+    pairs of nodes.
+
     Using netwrokx module:
     -----
     >>> import networkx as nx
@@ -64,24 +61,42 @@ class ExtendedGraph(nx.Graph):
     [(1, 2)]
     """
     def rcontract_min_cut(self):
+        # TODO: test this
         """
-        David Karger's Random Contraction Algorithm
+        David Karger's '90s Random Contraction Algorithm
 
         -    While there are more than 2 vertices:
-        i.    Pick a remaining edge (u,v) uniformly at random.
-        ii.    Merge (or “contract”) u and v into a single vertex.
-        iii.    Remove self-loops
-        iv.    Return cut represented by final 2 vertices.
+            i.    Pick a remaining edge (u,v) uniformly at random.
+            ii.    Merge (or “contract”) u and v into a single vertex.
+            iii.   Remove self-loops
+        -    Return cut represented by final 2 vertices.
         """
 
-        # TODO: create this algorithm
-        return 0
-        pass
+        min_cut = float('inf')
+        temp_nodes = self.nodes()
+        temp_edges = self.edges()
+        while temp_edges > 2:
+            rchoice(temp_edges)
+            # TODO: create this algorithm
+            pass
+
+        min_cut = len(temp_edges)
+        return min_cut
 
     def get_min_cut(self):
+        # TODO: test this
         min_cut = None
+        len_nodes = len(self.nodes())
+
+        # Repetition = n^2*ln(n) -> p[fail]=1/n
+        if len_nodes <= 10:
+            REPETITION = 1000
+        else:
+            REPETITION = ceil((pow(len_nodes, 2)*log(len_nodes)))
+
+        # run randomized contraction + save best min_cut result
         for _ in range(REPETITION):
-            temp_min_cut = rcontract_min_cut(graph)
+            temp_min_cut = self.rcontract_min_cut()
             if temp_min_cut < min_cut:
                 min_cut = temp_min_cut
         return min_cut
@@ -89,27 +104,36 @@ class ExtendedGraph(nx.Graph):
     @staticmethod
     def init_graph_wstrings(node_data_strings):
         """
-        Initialize a new ExtendedGraph:
+        Initialize a new ExtendedMultiGraph:
         Accepts a list of strings, each holding information regarding
         a single node (first element) and its connections (separated by spaces)
-        Returns a graph_object of type ExtendedGraph(nx.Graph)
+        Returns a graph_object of type ExtendedMultiGraph(nx.MultiGraph)
         Holding node and edge information of the input data.
         """
-        graph_object = ExtendedGraph()
+        graph_object = ExtendedMultiGraph()
         for line in node_data_strings:
+            assigned_node = False
             line_elements = list(map(int, line.split()))
-            graph_object.add_node(line_elements[0])
+            if line_elements[0] in graph_object.nodes():
+                assigned_node = True
+            else:
+                graph_object.add_node(line_elements[0])
             for element_index in range(1, len(line_elements)):
+                # if edge already listed, skip
+                if (assigned_node and line_elements[element_index]
+                        in graph_object.nodes()):
+                    continue
+                # else add new edge
                 (graph_object
                  .add_edge(line_elements[0], line_elements[element_index]))
         return graph_object
 
 
-class ExtendedGraphTestCase(unittest.TestCase):
+class ExtendedMultiGraphTestCase(unittest.TestCase):
     """Tests for `rcontract_min_cut.py`"""
 
     def test_networkx_module(self):
-        my_graph = ExtendedGraph()
+        my_graph = ExtendedMultiGraph()
         my_graph.add_edge(1, 2)
         my_graph.add_node(42)
         self.assertEqual(sorted(my_graph.nodes()), [1, 2, 42])
@@ -123,15 +147,35 @@ class ExtendedGraphTestCase(unittest.TestCase):
                           "4    1"
                           ]
 
-        my_graph = ExtendedGraph.init_graph_wstrings(adjacency_data)
+        my_graph = ExtendedMultiGraph.init_graph_wstrings(adjacency_data)
 
         self.assertEqual(sorted(my_graph.nodes()), [1, 2, 3, 4])
         self.assertEqual(sorted(my_graph.edges()), [(1, 3), (1, 4), (2, 3)])
 
+#     def test_get_min_cut(self):
+# 
+#         my_graph1 = ExtendedMultiGraph()  # empty graph
+#         min_cut1 = my_graph1.get_min_cut()
+#         self.assertEqual(min_cut1, 0)
+# 
+#         my_graph2 = ExtendedMultiGraph()  # single node graph
+#         my_graph2.add_edge(1, 1)
+#         self.assertEqual(sorted(my_graph2.nodes()), [1])
+#         self.assertEqual(sorted(my_graph2.edges()), [(1, 1)])
+#         min_cut2 = my_graph1.get_min_cut()
+#         self.assertEqual(min_cut2, 0)
+# 
+#         my_graph3 = ExtendedMultiGraph()  # isolated node
+#         my_graph3.add_edge(1, 2)
+#         my_graph3.add_node(42)
+#         self.assertEqual(sorted(my_graph3.nodes()), [1, 2, 42])
+#         self.assertEqual(sorted(my_graph3.edges()), [(1, 2)])
+#         min_cut3 = my_graph1.get_min_cut()
+#         self.assertEqual(min_cut3, 0)
+
     def test_somthing(self):
         # TODO: implement some tests
         pass
-
 
 
 
@@ -143,9 +187,9 @@ def main(file_name):
 
         # populate graph
         node_data_strings = [line.strip() for line in fh]
-        my_graph = ExtendedGraph.init_graph_wstrings(node_data_strings)
+        my_graph = ExtendedMultiGraph.init_graph_wstrings(node_data_strings)
 
-        min_cut = get_min_cut(my_graph)
+        min_cut = my_graph.get_min_cut()
 
         print(min_cut)
 
